44549c3a6ea3cdbc9b4cf62d75352b3d075c648d
Shreshu <abdshubham13@gmail.com>
2022-07-29 18:00:06 +0530

    framer motion added

---
+++ src/screen/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import logo from "../assests/logo.jpg";
+import { Example } from "../MenuMotion/Example";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <Link to="/" className="header__logo">
+          <img src={logo} alt="" />
+        </Link>
+        <Link to="/menu" className="header__link">
+          Menu
+        </Link>
+        <Link to="/menu" className="header__link">
+          Menu
+        </Link>
+        <Link to="/menu" className="header__link">
+          Menu
+        </Link>
+      </div>
+      <div className="header__right">
+        <Example />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
